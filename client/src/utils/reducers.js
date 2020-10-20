c67b522a7c520ac256f46eca5c34112a44eb3555
JoeL <jlawton1203@gmail.com>
2020-10-20 12:35:08 -0500

    Implemented tests related to shopping cart actions. Implemented the Cart and CartItem components and implemented functionality for the cart including adding items to the cart, removing items from the cart, etc.

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -3,7 +3,13 @@ import {useReducer} from 'react';
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
 } from './actions'
 
 export const reducer = (state, action) => {
@@ -26,6 +32,56 @@ export const reducer = (state, action) => {
                 ...state,
                 currentCategory: action.currentCategory,
             }
+        //if action type value is the value of `ADD_TO_CART`, return a new state object with an updated cart array
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.product]
+            };
+        //if action type value is the value of `ADD_MULTIPLE_TO_CART`, return a new state object with an updated cart array
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products],
+            };
+        //if action type value is the value of `REMOVE_FROM_CART`, return a new state object with an updated cart array
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter(product => {
+                return product._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+            };
+        //if action type value is the value of `UPDATE_CART_QUANTITY`, return a new state object with an updated cart array
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map(product => {
+                    if (action._id === product._id) {
+                        product.purchaseQuantity = action.purchaseQuantity;
+                    }
+                    return product;
+                })
+            };
+
+        //if the action type value is the value of `CLEAR_CART`, return a new state object with an empty cart array
+        case CLEAR_CART:
+            return {
+                ...state,
+                cartOpen: false,
+                cart: []
+            };
+        //if the action type value is the value of `TOGGLE_CART`, return a new state object with an updated cartOpen value
+        case TOGGLE_CART:
+            return {
+                ...state,
+                cartOpen: !state.cartOpen
+            };
         //if it's none of these actions, do not update state at all and keep things the same!
         default:
             return state;
